1131456866608f1ecbb2830fce82b68d8f2f2808
LicetLemus <161054695+LicetLemus@users.noreply.github.com>
2024-06-07 13:34:33 -0500

    style: Se agrego estilos a li (flexbox), se modifican algunas clase en view

--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@ import { renderItems } from './view.js';
 
 import data from './data/dataset.js';
 
+
 //console.log(example, renderItems(data), data);
 
-const ulElement = renderItems(data);
 const mainElement = document.getElementById('root');
+mainElement.innerHTML = '';    //Para limpiar el main del ul previo
+
+const ulElement = renderItems(data);
 mainElement.appendChild(ulElement);
