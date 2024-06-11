d7b96cf2061bc0b58f1512d00f3009eaefc20c6f
Aben <126703861+abengl@users.noreply.github.com>
2024-06-11 18:03:41 -0500

    se corrigió el error del test de css, se actualizó el view.j con la estructura html en los li y se modificó los selectores y estilos en styles.css

--- src/main.js
+++ src/main.js
@@ -2,14 +2,6 @@
 import { renderItems } from "./view.js";
 import data from "./data/dataset.js";
 
-//console.log(example, renderItems(data), data);
-
 const mainElement = document.getElementById("root");
-
-/* Para remover el ul por default creado en index.html */
-const ulDefault = document.querySelector("ul");
-mainElement.removeChild(ulDefault);
-
-/* Creando un nuevo ul y sus li con renderItems*/
 const ulElement = renderItems(data);
 mainElement.appendChild((ulElement));
--- src/view.js
+++ src/view.js
@@ -1,29 +1,25 @@
 export const renderItems = (data) => {
-  // Creamos el elemento ul
   const ulElement = document.createElement("ul");
 
   data.forEach((objeto) => {
-    //Creamos cada li
     const liElement = document.createElement("li");
-
-    //Añadimos las clases y atributos al li
     liElement.classList.add("card");
     liElement.setAttribute("itemscope", "");
     liElement.setAttribute("itemtype", "TecnologiasDeProgramacion");
-    
+
     liElement.innerHTML = `
         <div class="card-image">
-        <img src="${objeto.imageUrl}" alt="${objeto.name}" />
+          <img class="image" src="${objeto.imageUrl}" alt="${objeto.name}" />
+          <span class="card-label type" itemprop="type">${objeto.facts.type}</span>
+          <img src="assets/power.svg" alt="difficulty level" class="icon ${objeto.facts.difficultyLevel}" itemprop="difficultyLevel" />
+          <span class="card-label field" itemprop="aplicationField">${objeto.facts.aplicationField}</span>
         </div>
         <div class="card-content">
-        <span class="card-label type" itemprop="type">${objeto.facts.type}</span>
-        <span class="card-label difficultyLevel" itemprop="difficultyLevel">${objeto.facts.difficultyLevel}</span>
-        <span class="card-label field" itemprop="aplicationField">${objeto.facts.aplicationField}</span>
-        <h3 class="card-text name" itemprop="name">${objeto.name}</h3>
-        <p class="card-text description" itemprop="description">${objeto.description}</p>
-        <span class="card-text year" itemprop="yearOfCreation">Creado en: ${objeto.facts.yearOfCreation}</span>
-        <span class="card-text creator" itemprop="creator">Autor: ${objeto.facts.creator}</span>
-        <span class="card-text users" itemprop="percentageOfUsers">${objeto.facts.percentageOfUsers}</span>
+          <h3 class="card-text name" itemprop="name">${objeto.name}</h3>
+          <p class="card-text description" itemprop="description">${objeto.description}</p>
+          <span class="card-text year" itemprop="yearOfCreation">Creado en: ${objeto.facts.yearOfCreation}</span>
+          <span class="card-text creator" itemprop="creator">Autor: ${objeto.facts.creator}</span>
+          <span class="card-text users" itemprop="percentageOfUsers">${objeto.facts.percentageOfUsers}</span>
         </div>
       `;
 
