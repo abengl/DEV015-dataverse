8b63186aa1c9b4cc3c8fbc19b6c3b689a5edfdb0
Aben <126703861+abengl@users.noreply.github.com>
2024-06-10 19:12:40 -0500

    updates on index.html, view.js, main.js to correct the test error, updates on style.css accordingly

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 //import { example } from './dataFunctions.js';
 import { renderItems } from "./view.js";
-
 import data from "./data/dataset.js";
 
 //console.log(example, renderItems(data), data);
@@ -9,15 +8,8 @@ const mainElement = document.getElementById("root");
 
 /* Para remover el ul por default creado en index.html */
 const ulDefault = document.querySelector("ul");
-ulDefault.parentNode.removeChild(ulDefault);
+mainElement.removeChild(ulDefault);
 
 /* Creando un nuevo ul y sus li con renderItems*/
 const ulElement = renderItems(data);
-mainElement.appendChild(ulElement);
-
-/* Variables para revisar la clase de los li */
-const liElements = document.querySelectorAll(".card");
-const allHaveClass = Array.from(liElements).every((li) =>
-  li.classList.contains("card")
-);
-console.log('Todos los li tienen la clase "card":', allHaveClass);
+mainElement.appendChild((ulElement));
--- src/view.js
+++ src/view.js
@@ -1,13 +1,16 @@
 export const renderItems = (data) => {
   // Creamos el elemento ul
   const ulElement = document.createElement("ul");
-  //Iteramos la data
+
   data.forEach((objeto) => {
     //Creamos cada li
     const liElement = document.createElement("li");
+
+    //Añadimos las clases y atributos al li
     liElement.classList.add("card");
     liElement.setAttribute("itemscope", "");
     liElement.setAttribute("itemtype", "TecnologiasDeProgramacion");
+    
     liElement.innerHTML = `
         <div class="card-image">
         <img src="${objeto.imageUrl}" alt="${objeto.name}" />
@@ -15,12 +18,12 @@ export const renderItems = (data) => {
         <div class="card-content">
         <span class="card-label type" itemprop="type">${objeto.facts.type}</span>
         <span class="card-label difficultyLevel" itemprop="difficultyLevel">${objeto.facts.difficultyLevel}</span>
-        <span class="card-text name" itemprop="name">${objeto.name}</span>
-        <span class="card-text description" itemprop="description">${objeto.description}</span>
+        <span class="card-label field" itemprop="aplicationField">${objeto.facts.aplicationField}</span>
+        <h3 class="card-text name" itemprop="name">${objeto.name}</h3>
+        <p class="card-text description" itemprop="description">${objeto.description}</p>
         <span class="card-text year" itemprop="yearOfCreation">Creado en: ${objeto.facts.yearOfCreation}</span>
         <span class="card-text creator" itemprop="creator">Autor: ${objeto.facts.creator}</span>
         <span class="card-text users" itemprop="percentageOfUsers">${objeto.facts.percentageOfUsers}</span>
-        <span class="card-label field" itemprop="aplicationField">${objeto.facts.aplicationField}</span>
         </div>
       `;
 
