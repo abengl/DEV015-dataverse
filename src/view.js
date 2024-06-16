0708315d290af6d98c4a2729b19ed9f2797f4d79
Aben <126703861+abengl@users.noreply.github.com>
2024-06-16 12:10:57 -0500

    update in microdate itemprop to match schema specification and index.html modification of test attribute name to avoid confilct in e2e test

--- src/view.js
+++ src/view.js
@@ -5,21 +5,21 @@ export const renderItems = (data) => {
     const liElement = document.createElement("li");
     liElement.classList.add("card");
     liElement.setAttribute("itemscope", "");
-    liElement.setAttribute("itemtype", "TecnologiasDeProgramacion");
+    liElement.setAttribute("itemtype", "https://schema.org/CreativeWork");
 
     liElement.innerHTML = `
         <div class="card-image">
-          <img class="image" src="${objeto.imageUrl}" alt="${objeto.name}" />
-          <span class="card-label type" itemprop="type">${objeto.facts.type}</span>
-          <img src="assets/fire.svg" alt="difficulty level" class="icon ${objeto.facts.difficultyLevel}" itemprop="difficultyLevel" />
-          <span class="card-label field" itemprop="applicationField">${objeto.facts.applicationField}</span>
+          <img class="image" src="${objeto.imageUrl}" alt="${objeto.name}" itemprop="image"/>
+          <span class="card-label type" itemprop="additionalType">${objeto.facts.type}</span>
+          <img src="assets/fire.svg" alt="difficulty level" class="icon ${objeto.facts.difficultyLevel}" itemprop="educationalLevel" />
+          <span class="card-label field" itemprop="exampleOfWork">${objeto.facts.applicationField}</span>
         </div>
         <div class="card-content">
           <h3 class="card-text name" itemprop="name">${objeto.name}</h3>
           <p class="card-text description" itemprop="description">${objeto.description}</p>
-          <span class="card-text year" itemprop="yearOfCreation">Creado en: ${objeto.facts.yearOfCreation}</span>
+          <span class="card-text year" itemprop="dateCreated">Creado en: ${objeto.facts.yearOfCreation}</span>
           <span class="card-text creator" itemprop="creator">Autor: ${objeto.facts.creator}</span>
-          <span class="card-text users" itemprop="percentageOfUsers">${objeto.facts.percentageOfUsers}</span>
+          <span class="card-text users" itemprop="usageInfo">${objeto.facts.percentageOfUsers}</span>
         </div>
       `;
 
