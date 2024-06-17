768340ad016bf9ff580a9f504f9d02094bfb7ef9
LicetLemus <161054695+LicetLemus@users.noreply.github.com>
2024-06-17 15:25:25 -0500

    button-clear

--- src/main.js
+++ src/main.js
@@ -22,12 +22,19 @@ document.addEventListener("DOMContentLoaded", () => {
   const filterSelectType = document.querySelector("#type-select");
   const filterSelectApplication = document.querySelector("#applicationField-select"); 
   const orderSelect = document.querySelector("#order-select");
+  const clearButton = document.querySelector('[data-testid="button-clear"]')
 
   filterSelectType.addEventListener("change", (event) => {
     const selectedValueType = event.target.value;
     const filterItemsType = filterData(data, "type", selectedValueType);
     resetSelectIndex(filterSelectApplication);
     displayCards(filterItemsType);
+
+    orderSelect.addEventListener("change",(event) => {
+      const selectedValueOrder = event.target.value;
+      const orderItemsName = sortData(filterItemsType, "name", selectedValueOrder);
+      displayCards(orderItemsName);
+    }); 
   });
 
   filterSelectApplication.addEventListener("change", (event) => {
@@ -35,6 +42,13 @@ document.addEventListener("DOMContentLoaded", () => {
     const filterItemsApplication = filterData(data, "applicationField", selectedValue);
     resetSelectIndex(filterSelectType);
     displayCards(filterItemsApplication);
+
+    orderSelect.addEventListener("change",(event) => {
+      const selectedValueOrder = event.target.value;
+      const orderItemsName = sortData(filterItemsApplication, "name", selectedValueOrder);
+      displayCards(orderItemsName);
+    });
+
   });
 
   orderSelect.addEventListener("change",(event) => {
@@ -42,4 +56,10 @@ document.addEventListener("DOMContentLoaded", () => {
     const orderItemsName = sortData(data, "name", selectedValueOrder);
     displayCards(orderItemsName);
   }); 
+
+  clearButton.addEventListener("click", function () {
+    mainElement.removeChild(ulElement);
+    ulElement = renderItems(data);
+    mainElement.appendChild(ulElement);
+  });
 });
