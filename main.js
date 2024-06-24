2086939e66834a3b8724191b56a7d3a11c2c994d
Aben <126703861+abengl@users.noreply.github.com>
2024-06-24 11:12:02 -0500

    Updates

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import { filterData, sortData, computeStats } from "./dataFunctions.js";
-import { renderItems } from "./view.js";
+import { renderItems, renderRanking } from "./view.js";
 import data from "./data/dataset.js";
+
 
 const mainElement = document.getElementById("root");
 let ulElement = renderItems(data);
@@ -70,13 +71,18 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   clearButton.addEventListener("click", () => {
-    mainElement.removeChild(ulElement);
-    ulElement = renderItems(data);
-    mainElement.appendChild(ulElement);
+    displayCards(data);
   });
 
   metricsButton.addEventListener("click", () => {
     const metricsItems = computeStats(data);
-    displayCards(metricsItems);
+    mainElement.removeChild(ulElement);
+    ulElement = renderRanking(metricsItems);
+    mainElement.appendChild(ulElement);
+    const h3Elements = document.querySelectorAll(".title-hover");
+    console.log(h3Elements);
+    h3Elements[0].innerHTML = "Lenguage de Programación Más Usado";
+    h3Elements[1].innerHTML = "Lenguage de Programación Más Antiguo";
+    h3Elements[2].innerHTML = "Lenguage de Programación Más Reciente";
   });
 });
